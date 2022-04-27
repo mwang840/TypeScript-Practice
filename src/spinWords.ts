c6bcb7fc4311d3e477ceeaa42b78d2923d4a513f
Maxwell Wang <maxwang@udel.edu>
2022-04-27 05:40:56 -0400

    structured a loop so far

---
+++ src/spinWords.ts
@@ -0,0 +1,14 @@
+export function spinWords(words: string): string {
+    let spinned = "";
+    for(let i = 0; i < words.length; ++i){
+        if(words.charAt(i) !== ' '){
+            if(words[i].length >= 5){
+                for(let j = words[i].length; j >= 0; ++j){
+                    
+                }
+            }
+        }
+    }
+    return spinned;
+    //TODO Have fun :)
+}
