f3e500862183fc1c173b302608f2fcce0c0287ec
Maxwell Wang <maxwang@udel.edu>
2022-04-28 08:11:55 -0400

    cleared up syntax for spinning words

--- src/spinWords.ts
+++ src/spinWords.ts
@@ -1,13 +1,18 @@
 export function spinWords(words: string): string {
     let spinned = "";
-    for(let i = 0; i < words.length; ++i){
-        if(words.charAt(i) !== ' '){
-            if(words[i].length >= 5){
-                for(let j = words[i].length; j >= 0; ++j){
-                    
+    if(spinned.split(" ").includes(words)){
+        for(let i = 0; i < words.length; ++i){
+            if(words.charAt(i) !== ' '){
+                if(words[i].length >= 5){
+                    for(let j = words[i].length; j >= 0; --j){
+                      spinned += words[j];  
+                    }
                 }
             }
         }
+    }
+    else{
+        spinned = words;
     }
     return spinned;
     //TODO Have fun :)
