fc6bd7f24a660d6131677013cc3c4b39a2462232
Maxwell Wang <maxwang@udel.edu>
2022-04-30 21:56:49 -0400

    sort method

--- src/decendingOrder.ts
+++ src/decendingOrder.ts
@@ -1,16 +1,13 @@
 export function descendingOrder(n: number): number {
     let descending = String(n);
     let popular = [];
+    //Turns the numbers to an string then put each digit out to an array
     for(let i = 0; i < descending.length; ++i){
       popular.push(descending[i]);
     }
-
-    for(let j = 0; j < popular.length + 1; ++j){
-      if(popular[j] < popular[j+1]){
-        popular[j] = popular[j+1];
-      }
-    }
-    
+    //Shallow copy first, then sorts the array from greatest to least (ternary to check if one > two; place one first; otherwise
+    //put the other first and repeat until end of array)
+    [...popular].sort((one, two)=>(one > two ? -1: 1));
+    //convert it to a number then return the number
     return Number(popular);
-    // your code here
   }
