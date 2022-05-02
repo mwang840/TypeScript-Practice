e9deeb59a8d748b21c2284865e4cb7327ee16f08
Maxwell Wang <maxwang@udel.edu>
2022-05-02 08:52:50 -0400

    fixed name in file and added functions to main

--- src/decendingOrder.ts
+++
@@ -1,8 +0,0 @@
-export function descendingOrder(n: number): number {
-    //Converts to string
-    let descending = n.toString();
-    //Splits it to an array by char
-    let arr = descending.split("");
-    //ParseInts the current array while sorting it in reverse order then getting rid of the gaps
-    return parseInt(arr.sort().reverse().join(""));
-}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { spinWords } from "./spinWords";
-
-function main(){
+import { descendingOrder } from "./descendingOrder";
+function main() {
     const fat = spinWords("Nikocado Avocado");
     console.log(fat);
+
+    const decreasing = 5698;
+    console.log(descendingOrder(5698));
 }
