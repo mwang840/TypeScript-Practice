18d382fc7710d94cbe17433fd43e30684e13e9c1
Maxwell Wang <maxwang@udel.edu>
2022-05-01 21:08:54 -0400

    decending order done in less than 10 lines ;)

--- src/decendingOrder.ts
+++ src/decendingOrder.ts
@@ -1,13 +1,8 @@
 export function descendingOrder(n: number): number {
+    //Converts to string
     let descending = n.toString();
-    let popular = [];
-    //Turns the numbers to an string then put each digit out to an array
-    for(let i = 0; i < descending.length; ++i){
-      popular.push(descending[i]);
-    }
-    //Shallow copy first, then sorts the array from greatest to least (ternary to check if one > two; place one first; otherwise
-    //put the other first and repeat until end of array)
-    [...popular].sort((one, two)=>(one > two ? -1: 1));
-    //convert it to a number then return the number
-    return Number(popular);
-  }
+    //Splits it to an array by char
+    let arr = descending.split("");
+    //ParseInts the current array while sorting it in reverse order then getting rid of the gaps
+    return parseInt(arr.sort().reverse().join(""));
+}
